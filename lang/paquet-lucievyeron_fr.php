1ff6e35e359a0f64e91c9e0ee8ccf4b67d318e8e
webetcie <83359753+webetcie@users.noreply.github.com>
2025-12-10 11:54:51 +0100

    Création du plugin

---
+++ lang/paquet-lucievyeron_fr.php
@@ -0,0 +1,14 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// O
+	'lucievyeron_description' => '',
+	'lucievyeron_nom' => 'Lucie Veyron',
+	'lucievyeron_slogan' => 'Templates pour le site Lucie Veyron',
+);
---
+++ squelettes/svg/index.php
@@ -0,0 +1,5 @@
+<?php
+header("Status: 301 Moved Permanently", false, 301);
+header("Location: https://www.psychologue-enfant-56.fr/");
+exit();
+?> 
